e12dde943e2921563a98769dcc884f8869515669
Mark Melnic <markhappy200@gmail.com>
2021-03-09 15:42:59 +0200

    DDR1 implemented

--- rules.m
+++ rules.m
@@ -5,3 +5,20 @@ function [ fncs ] = rules()
         fncs{i} = str2func(fncs{i});
     end
 end
+
+%DDR1 Degree of responsibility
+function result = ddr1(trace, params, t)
+    result = {};
+    for ability_to_help = l2.getall(trace, t, 'ability_to_help', {NaN})
+        ability = ability_to_help.arg{1};
+        for number_of_bystanders = l2.getall(trace, t, 'number_of_bystanders', {NaN})
+            bystanders = number_of_bystanders.arg{1};
+            if ability && bystanders > 0
+                responsibility_value = ability / bystanders;
+                result = {result{:} {t+1, 'degree_of_responsibility', {responsibility_value}}};
+            end
+        end 
+    end
+end
+
+%DDR2 Interpretation of robbery 
